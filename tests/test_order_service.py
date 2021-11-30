321c9afa1416d9db420b30dce586f3cf67cf4d7d
Erik Nakata <erik.nakata5@gmail.com>
2021-11-30 20:20:09 -0300

    Refactor test_order_service

--- tests/test_order_service.py
+++ tests/test_order_service.py
@@ -1,25 +1,28 @@
 import pytest
 from decimal import Decimal
-from unittest.mock import MagicMock
+
+from src.order_repository import OrderRepository
 from src.order_service import OrderService
+from tests.integration_test import IntegrationTest
+from tests.order_factory import OrderFactory
 
 
-class TestOrderService:
+class TestOrderService(IntegrationTest):
 
     @pytest.fixture
-    def order_repository(self):
-        return MagicMock()
+    def order_repository(self, session):
+        return OrderRepository(session)
 
     @pytest.fixture
     def order_service(self, order_repository):
         return OrderService(order_repository)
 
     def test_should_return_the_total_by_customer(self, order_service):
-        order_service.order_repository.sum_customer_orders = MagicMock(return_value=Decimal(100.00))
+        OrderFactory.create_batch(size=5, customer_email='test@gmail.com', total=20.00)
 
-        assert order_service.get_total_orders_by_user('test@gmail.com') == Decimal(100.00)
+        total = order_service.get_total_orders_by_user('test@gmail.com')
+
+        assert total == Decimal(100.00)
 
     def test_should_return_zero_when_customer_has_no_orders(self, order_service):
-        order_service.order_repository.sum_customer_orders = MagicMock(return_value=Decimal(0.00))
-
         assert order_service.get_total_orders_by_user('test@gmail.com') == Decimal(0.00)
